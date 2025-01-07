be01f59d514830f3323d3987d1edf5a50efe35c8
Anshul Rai <96529476+anshulraigh@users.noreply.github.com>
2025-01-07 22:04:21 +0530

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class SimpleInterest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-		System.out.prinln("Enter principal amount");
+		System.out.println("Enter principal amount");
         double principal = scanner.nextDouble();
-		System.out.prinln("Enter rate of Interest");
+		System.out.println("Enter rate of Interest");
         double rate = scanner.nextDouble();
-		System.out.prinln("Enter time in year");
+		System.out.println("Enter time in year");
         double time = scanner.nextDouble();
         double simpleInterest = (principal * rate * time) / 100;
         System.out.println("The simple interest is " + simpleInterest);
